a459e0b8ef25bb12fab8f50d6d2461f0e5c7bbcb
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-09-25 14:40:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const exphbs = require('express-handlebars')
-const hbs = exphbs.create({})
-const path = require('path');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,10 +11,16 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 1 * 60 * 60 * 1000,
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
   },
   resave: false,
   saveUninitialized: true,
@@ -24,13 +30,14 @@ const sess = {
 };
 
 app.use(session(sess));
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
